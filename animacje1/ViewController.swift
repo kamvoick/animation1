e5ea80bff6859b3d76688fbc1c9ecbe3b0b15d13
Kamil Wójcik <van_kam@wp.pl>
2016-06-24 05:23:13 +0200

    reload

--- animacje1/ViewController.swift
+++ animacje1/ViewController.swift
@@ -10,14 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var witamLbl: UILabel!
+    @IBOutlet weak var drugiLabel: UILabel!
+    @IBOutlet weak var trzeciLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidLayoutSubviews() {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        witamLbl.center.y -= view.bounds.height
+        drugiLabel.center.y += view.bounds.height
+        trzeciLabel.alpha = 0.0
+        trzeciLabel.textColor = UIColor.whiteColor()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //animujemy witam
+        UIView.animateWithDuration(1.0, animations: {
+            
+            self.witamLbl.center.y += self.view.bounds.height
+            
+            }, completion: { finished in
+                self.drugaAnimacja()
+        })
+        
+        //animujemy tło
+        UIView.animateWithDuration(2.0, delay: 0.5, options: [], animations: {
+            
+            self.view.backgroundColor = UIColor.yellowColor()
+            
+            }, completion: { finished in
+                self.kolorTła()
+        })
+    }
+    
+    func drugaAnimacja(){
+        UIView.animateWithDuration(1.0, delay: 0.0, options: [], animations: { 
+            
+            self.drugiLabel.center.y -= self.view.bounds.height
+
+            
+            }, completion: nil)
+    }
+
+    func kolorTła(){
+        UIView.animateWithDuration(2.5, animations: { 
+            
+            self.view.backgroundColor = UIColor.blackColor()
+            }, completion: nil)
+        
+        UIView.animateWithDuration(1.0, delay: 1.5, options: [], animations: {
+            
+            self.trzeciLabel.alpha = 1.0
+            }, completion: nil)
     }
 
 
